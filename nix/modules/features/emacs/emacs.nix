e13dfe504e67c3c91814b80131fe5816bfdb9c07
eoghanriley <me@eoghanriley.dev>
2026-08-05 09:45:33 -0400

    wip

---
+++ nix/modules/features/emacs/emacs.nix
@@ -0,0 +1,15 @@
+{ self, inputs, ... }: {
+ flake.nixosModules.emacs = { pkgs, lib, ... }: {
+    environment.systemPackages = [
+      self.packages.${pkgs.stdenv.hostPlatform.system}.myEmacs
+    ];
+  };
+
+  perSystem = { pkgs, ... }: {
+    packages.myEmacs = 
+     (pkgs.extend inputs.nix-doom-emacs-unstraightened.overlays.default).emacsWithDoom {
+      doomDir = ./;
+      doomLocalDir = "~/.local/share/nix-doom";
+    };
+  };
+}
