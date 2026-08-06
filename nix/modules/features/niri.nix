5d4ebd99935cdb425cedfd87bace2060c0708237
eoghanriley <me@eoghanriley.dev>
2026-08-05 20:41:16 -0400

    fix: fix broken ghostty call

--- nix/modules/features/niri.nix
+++ nix/modules/features/niri.nix
@@ -27,38 +27,38 @@
 	      # Launch applications
           "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
 
-	     # Manage windows
-         "Mod+W".close-window = {};
-	     "Mod+MouseMiddle".close-window = {};
+	      # Manage windows
+          "Mod+W".close-window = {};
+	      "Mod+MouseMiddle".close-window = {};
 
-	     "Mod+F".fullscreen-window = {};
+	      "Mod+F".fullscreen-window = {};
 
-	     "Mod+minus".set-column-width = "-10%";
-	     "Mod+equal".set-column-width = "+10%";
-	     "Mod+Shift+minus".set-window-height = "-10%";
-	     "Mod+Shift+equal".set-window-height = "+10%";
+	      "Mod+minus".set-column-width = "-10%";
+	      "Mod+equal".set-column-width = "+10%";
+	      "Mod+Shift+minus".set-window-height = "-10%";
+	      "Mod+Shift+equal".set-window-height = "+10%";
 
-	     # Movement
-	     "Mod+bracketleft".focus-column-left = {};
-	     "Mod+bracketright".focus-column-right = {};
-	     "Mod+braceleft".focus-window-up = {};
-	     "Mod+braceright".focus-window-down = {};
+	      # Movement
+	      "Mod+bracketleft".focus-column-left = {};
+	      "Mod+bracketright".focus-column-right = {};
+	      "Mod+braceleft".focus-window-up = {};
+	      "Mod+braceright".focus-window-down = {};
 
-	     "Mod+Left".move-column-left = {};
-	     "Mod+Right".move-column-right = {};
-	     "Mod+Up".move-window-up = {};
-	     "Mod+Down".move-window-down = {};
+	      "Mod+Left".move-column-left = {};
+	      "Mod+Right".move-column-right = {};
+	      "Mod+Up".move-window-up = {};
+	      "Mod+Down".move-window-down = {};
           
-	     # Util
-	     "Mod+Shift+S".screenshot = {};
-	     "Mod+Shift+W".screenshot-window = {};
+	      # Util
+	      "Mod+Shift+S".screenshot = {};
+	      "Mod+Shift+W".screenshot-window = {};
 
-	     "Mod+Space".toggle-overview = {};
+	      "Mod+Space".toggle-overview = {};
 
-         spawn-at-startup = [
-	       (lib.getExe self'.packages.myNoctalia)
-	     ];
-         "Alt+Space".spawn-sh =
+          spawn-at-startup = [
+	        (lib.getExe self'.packages.myNoctalia)
+	      ];
+          "Alt+Space".spawn-sh =
 	       "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
         };
       };
